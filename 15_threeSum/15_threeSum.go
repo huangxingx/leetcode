2a335557fdcd4e356284a4105131dc03d0e860e3
huangxing <huangxing@arhieason.com>
2019-10-11 15:56:10 +0800

    feature# add request 和 解题思路

--- 15_threeSum/15_threeSum.go
+++ 15_threeSum/15_threeSum.go
@@ -5,13 +5,25 @@ import (
 	"sort"
 )
 
+/*
+数组排序
+两层循环, 外层循环遍历整个数组, 内层循环通过两个游标获取首尾数据
+if sum < 0 , 递增左边的游标
+if sum > 0 , 递增右边的游标
+else append 三个数的数组
+
+注意数据的去重处理
+ */
+
 func threeSum(nums []int) [][]int {
 	resultList := make([][]int, 0)
 
 	sort.Ints(nums)
 
 	for k := 0; k < len(nums)-1; k++ {
-		if nums[k] > 0 {break}
+		if nums[k] > 0 {
+			break
+		}
 
 		if k > 0 && nums[k] == nums[k-1] {
 			continue
