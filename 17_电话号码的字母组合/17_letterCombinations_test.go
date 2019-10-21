dd92222067a0c0d330e05db9e438a56f93d5b2af
huangxing <huangxing654123@163.com>
2019-10-21 11:07:50 +0800

    feature# 17. 电话号码的字母组合  <完成>

---
+++ 17_电话号码的字母组合/17_letterCombinations_test.go
@@ -0,0 +1,11 @@
+package letterCombinations
+
+import (
+	"testing"
+	"fmt"
+)
+
+func TestLetterCombinations(t *testing.T) {
+	//fmt.Println(letterCombinations("23"))
+	fmt.Println(letterCombinations(""))
+}
